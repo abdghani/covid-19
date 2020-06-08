ad36e70b248c9309dedad77f180dfa557af2e859
Your Name <you@example.com>
2020-06-08 20:15:53 +0530

    Updates

---
+++ precache-manifest.5ffff137eb6ae019396fd99c5ac40491.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6b2c1bce399e0bce87e0f3798a2bd8",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "85a2016ad6027b9679e9",
+    "url": "/covid-19/static/css/main.faa022d3.chunk.css"
+  },
+  {
+    "revision": "8e057ae5a3cae491ad22",
+    "url": "/covid-19/static/js/2.f4fbb355.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.f4fbb355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a2016ad6027b9679e9",
+    "url": "/covid-19/static/js/main.0d96df27.chunk.js"
+  },
+  {
+    "revision": "0d224c37ab1611d168ff",
+    "url": "/covid-19/static/js/runtime-main.9475372b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19/static/media/logo.5d5d9eef.svg"
+  }
+]);
